68dd17e69dbc2b369b257e8d3efbeadd6a41d35a
shyswy <shyswy@naver.com>
2025-04-23 14:24:32 +0900

    move crawl logic to spring batch + update dummy csv files

---
+++ youtubeBatch/src/main/kotlin/com/example/youtubeTrender/config/constant.kt
@@ -0,0 +1,16 @@
+package com.example.youtubeTrender.config
+
+object YoutubeConstants {
+    val REGIONS = listOf("KR", "US")
+
+    val CATEGORY_MAP = mapOf(
+        "all" to null,
+        "music" to "10",
+        "sports" to "17",
+        "people_blogs" to "22",
+        "comedy" to "23",
+        "entertainment" to "24",
+        "news" to "25",
+        // "travel" to "19",
+    )
+}
