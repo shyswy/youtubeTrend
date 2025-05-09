280aec74f52644c31086296ab865bc9df7e4e158
shyswy <shyswy@naver.com>
2025-05-09 18:49:29 +0900

    docker 환경변수로 batch 주기 조작가능하도록 수정

--- youtubeBatch/src/main/kotlin/com/example/youtubeTrender/batch/youtubeScheduler.kt
+++ youtubeBatch/src/main/kotlin/com/example/youtubeTrender/batch/youtubeScheduler.kt
@@ -6,6 +6,7 @@ import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
+import org.springframework.beans.factory.annotation.Value
 
 @Component
 class BatchScheduler(
@@ -17,11 +18,13 @@ class BatchScheduler(
     private lateinit var fixedRate: String
     // 데이터 양이 많을 경우, chunk-based processing과 같은 기법을 사용하여 성능을 최적화
     // https://king-ja.tistory.com/81.  Quartz 등 스케쥴러로 수정 시 retry 등 디테일 처리 가능.
-    @Scheduled(fixedRate = fixedRate.toLong())
+   @Scheduled(fixedRateString = "\${BATCH_JOB_FIXED_RATE:600000}") 
     fun runYoutubeCollectJob() {
         val jobParameters = org.springframework.batch.core.JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
             .toJobParameters()
+
+        println("batch 주기: ${fixedRate.toLong()}")
 
         println("⏰ YoutubeCollectJob 시작: ${LocalDateTime.now()}")
         jobLauncher.run(youtubeCollectJob, jobParameters)
